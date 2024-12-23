83f0297b139b488c98a8433265f44cd8b9a8634b
UllasCL <git@stackblitz.com>
2024-12-23 13:10:13 +0530

    Initial commit

---
+++ src/lib/Status.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let connectionStatus = '';
+  export let agentStatus = '';
+</script>
+
+<div class="status">
+  <p><span>{connectionStatus}</span></p>
+  <p><span>{agentStatus}</span></p>
+</div>
+
+<style>
+  .status {
+    font-size: 1.1em;
+    color: #555555;
+    margin-bottom: 10px;
+  }
+
+  span {
+    font-weight: bold;
+    color: #000000;
+  }
+</style>
